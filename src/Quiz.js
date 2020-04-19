3ab47930fffc46886002fcaa87b0abd4dc1fb57f
gummyman <gazisakib1995@gmail.com>
2020-04-18 21:15:10 -0400

    Created QuizEnd class and implemented it in Quiz class

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion.js';
+import QuizEnd from './QuizEnd.js';
 
 let quizData = require('./quiz_data.json')
 
@@ -12,10 +13,15 @@ constructor(props){
 
 
   render(){
+    const isQuizEnd = false;
+
     return (
     <div>
-      <QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position-1]}/>
-    </div> );
+    {(isQuizEnd)?
+    (<QuizEnd />):
+    (<QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position-1]}/>)}
+    </div>
+  )
   }
 
 }
